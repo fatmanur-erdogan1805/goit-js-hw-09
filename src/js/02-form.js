73c205bc6913bcbd77ec6eb4c3c113ceb2b3efe4
fatmanur-erdogan1805 <fatmanurerdogan1805@gmail.com>
2025-03-30 21:58:09 +0300

    commit

---
+++ src/js/02-form.js
@@ -0,0 +1,44 @@
+const form = document.querySelector('.feedback-form');
+
+//input event
+form.addEventListener('input', event => {
+  const email = form.elements.email.value.trim();
+  const message = form.elements.message.value.trim();
+  console.log({
+    email: email,
+    message: message,
+  });
+  // feedback-form-state
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({
+      email: email,
+      message: message,
+    })
+  );
+});
+
+const formDataLocal = JSON.parse(localStorage.getItem('feedback-form-state'));
+console.log(formDataLocal);
+console.log(typeof formDataLocal);
+
+if (formDataLocal !== null) {
+    form.elements.email.value = formDataLocal.email;
+form.elements.message.value = formDataLocal.message; 
+}
+form.addEventListener("submit", event => {
+     
+    event.preventDefault();
+    const email = form.elements.email.value;
+    const message = form.elements.message.value;
+    console.log({
+        email: email,
+        message: message,
+    });
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+
+    
+
+
+});
